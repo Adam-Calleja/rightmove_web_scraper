bcbf8f9421ff56af0234028070025b6c7aca439f
Adam Calleja <adampaulcalleja@gmail.com>
2025-02-28 11:16:26 +0000

    Create TestRightmoveWebsite test class
    Created the TestRightmoveWebsite test class which will test the
    RightmoveWebsite class.
    
    Added the following two unit tests for the 'get_location_identifier'
    method:
    	- test_get_location_identifier_correct_identifier
    	- test_get_location_identifier_correct_display_name

---
+++ Tests/TestRightmoveWebsite.py
@@ -0,0 +1,57 @@
+import pytest
+from rightmove_web_scraper.RightmoveWebsite import RightmoveWebsite
+
+class TestRightmoveWebsite():
+    """
+    Test Class for the RightmoveWebsite class
+    """
+
+    @pytest.fixture
+    def test_rightmovewebsite(self):
+        return TestRightmoveWebsite()
+    
+    def test_get_location_identifier_correct_identifier(self):
+        """
+        Test the get_location_identifier method
+
+        Tests that the get_location_identifier method returns
+        the correct location identifier for a given location.
+        Raises:
+        ------
+        AssertionError
+            If the location identifier is not equal to the expected value.
+        """
+
+        # Arrange
+        rightmove_website = RightmoveWebsite()
+        location = "manchester"
+
+        # Act
+        location_identifier, display_name = rightmove_website.get_location_identifier(location)
+
+        # Assert
+        assert location_identifier == "904"
+
+
+    def test_get_location_identifier_correct_display_name(self):
+        """
+        Test the get_location_identifier method
+
+        Tests that the get_location_identifier method returns
+        the correct and display name for a given location.
+
+        Raises:
+        ------
+        AssertionError
+            If the display name is not equal to the expected value.
+        """
+
+        # Arrange
+        rightmove_website = RightmoveWebsite()
+        location = "manchester"
+
+        # Act
+        location_identifier, display_name = rightmove_website.get_location_identifier(location)
+
+        # Assert
+        assert display_name == "Manchester, Greater Manchester"
