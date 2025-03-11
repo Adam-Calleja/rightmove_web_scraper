7265de82ebc585210b90dcb7b7907ce3257c6231
Adam Calleja <adampaulcalleja@gmail.com>
2025-03-11 23:53:42 +0000

    Modify the test classes to allow imports
    Added a command to the start of each of the unit test classes to
    allow the test classes to import modules from the parent directory
    (the rightmove_web_scraper package) without encoundering any
    import errors.

--- Tests/TestDatabase.py
+++ Tests/TestDatabase.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 import sqlite3
 from rightmove_web_scraper.Database import Database
