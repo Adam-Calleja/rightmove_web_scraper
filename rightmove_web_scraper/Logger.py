30cd73d0c37e04bf284e9c705650a7df0f4ecf99
Adam Calleja <adampaulcalleja@gmail.com>
2025-03-12 09:12:14 +0000

    Complete the Logger class
    Completed all of the source code of the Logger class. All unit
    tests in test_Logger.py now pass.

--- rightmove_web_scraper/Logger.py
+++ rightmove_web_scraper/Logger.py
@@ -1,3 +1,25 @@
+from datetime import datetime
+
 class Logger:
 
-    pass
+    def __init__(self, log_file: str) -> None:
+        if log_file:
+            self.log_file = log_file
+        else:
+            self.log_file = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+    def log_info(self, message: str) -> None:
+        with open(self.log_file, "a") as file:
+            file.write(f"[INFO] {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {message}\n")
+
+    def log_warning(self, message: str) -> None:
+        with open(self.log_file, "a") as file:
+            file.write(f"[WARNING] {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {message}\n")
+
+    def log_error(self, message: str) -> None:
+        with open(self.log_file, "a") as file:
+            file.write(f"[ERROR] {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {message}\n")
+
+    def log_debug(self, message: str) -> None:
+        with open(self.log_file, "a") as file:
+            file.write(f"[DEBUG] {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {message}\n")
